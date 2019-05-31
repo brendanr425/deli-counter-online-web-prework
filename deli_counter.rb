9dbe87963d6865619d86dce712f4446b99b09861
Brendan Robinson <brendanr425@gmail.com>
2019-05-31 16:09:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,4 @@
 # Write your code here.
-
-
 def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
@@ -21,9 +19,9 @@ end
 
 
 def now_serving(katz_deli)
-  if katz_deli.length == 0
-    puts "There is nobody waiting to be served."
-  else
+  if katz_deli.length != 0
     puts "Currently serving #{katz_deli.shift}"
+  else
+    puts "There is nobody waiting to be served."
   end
 end
