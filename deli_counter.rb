3592aa7ca09ecb2752e0a0a6395a45acc8eec2f0
Brendan Robinson <brendanr425@gmail.com>
2019-05-31 16:26:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,13 +7,13 @@ end
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
-    
+
   else
     katz_deli.each_with_index do |person, idx|
       puts "The line is currently: "
       print "#{idx}. #{person} "
     end
-    
+
   end
 end
 
